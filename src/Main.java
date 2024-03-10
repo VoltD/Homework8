63ca055a41175b2294fcaf134f2199f3337294cf
Dmitrii Volt <dmitriy.volt@gmail.com>
2024-03-10 12:45:04 +0300

    Tasks 3, 4

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         // Task 1
@@ -33,5 +35,41 @@ public class Main {
             }
         }
         System.out.println();
+        System.out.println();
+
+        // Task 3
+        System.out.println("Task 3");
+        for (int i = arr1.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.print(arr1[i]);
+            } else {
+                System.out.print(arr1[i] + ", ");
+            }
+        }
+        System.out.println();
+        for (int i = arr2.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.print(arr2[i]);
+            } else {
+                System.out.print(arr2[i] + ", ");
+            }
+        }
+        System.out.println();
+        for (int i = arr3.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.print(arr3[i]);
+            } else {
+                System.out.print(arr3[i] + ", ");
+            }
+        }
+        System.out.println();
+        System.out.println();
+
+        // Task 4
+        System.out.println("Task 4");
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] += 1;
+        }
+        System.out.println(Arrays.toString(arr1));
     }
 }
